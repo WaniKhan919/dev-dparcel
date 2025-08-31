0bb3ee7079306811d27650378f99ccec9601a97b
Wanikhan919 <wanikhan919@gmail.com>
2025-08-31 14:44:42 +0500

    config new template

--- src/heroicons.d.ts
+++
@@ -1,6 +0,0 @@
-// src/heroicons.d.ts
-declare module '@heroicons/react/solid' {
-  import * as React from 'react';
-  export const ChevronDownIcon: React.FC<React.SVGProps<SVGSVGElement>>;
-  // Add more icons here if you use them
-}
